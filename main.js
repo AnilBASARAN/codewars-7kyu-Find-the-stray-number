06d0638d18774160665e19fd0b57145206f7a314
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2022-05-27 23:34:26 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function stray(numbers) {
+    var a = numbers.sort();
+    
+    if(a[0] != a[1]) {
+      return a[0]
+    } 
+    return a[a.length-1]
+  }
+
+
+  /*
+7kyu- Find the stray number
+
+
+  You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+
+Examples
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3
+
+https://www.codewars.com/kata/57f609022f4d534f05000024/train/javascript
+
